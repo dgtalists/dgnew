08bef853101eab5b2e9a1dd9af908ea7ec76ae65
Arindam Sarkar <arindam@dgtalists.co.in>
2025-04-22 17:23:13 +0530

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Required for static HTML export
-  
+  trailingSlash: true, // Optional: adds a trailing slash to URLs
 };
 
 
