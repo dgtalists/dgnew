1cfc69892e8dad03040db0b8317b126a134313cb
Arindam Sarkar <arindam@dgtalists.co.in>
2025-06-20 14:28:42 +0530

    meta description add

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,29 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="LFigQReBdAgCpdgZ_NOM7o14JCcS-3FcPkoNq87317A"
+        />
+
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6QYCF7SBEL"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6QYCF7SBEL');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
